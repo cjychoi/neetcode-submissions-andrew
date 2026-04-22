f44226e7683d8bd09d195642534d5a2c9efb71bc
Andrew Junyoung Choi <cjychoi@gmail.com>
2026-04-21 23:12:30 -0500

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,18 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Brute Force
+        minK = max(piles)
+        # print("max = ", max(piles))
+        
+        for k in range(1, h):
+            print("k = ", k)
+            hours = 0
+            for i in piles:
+                hours = hours + math.ceil(i / k)
+                print("hours = ", hours)
+            if hours <= h:
+                minK = min(minK, k)
+                print("minK = ", minK)
+        return minK
